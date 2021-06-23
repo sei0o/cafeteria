5dc5b8d9e091c7dddbc743e7651723321e596587
sei0o <sei0o@live.jp>
2021-06-23 11:05:09 +0900

    some server-side redundant code

--- app.py
+++ app.py
@@ -1,7 +1,151 @@
-from flask import Flask
+import os
+
+from flask import Flask, render_template, request, flash, url_for, session
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
+from passlib.hash import bcrypt
 
 app = Flask(__name__)
+app.config.from_mapping(
+SECRET_KEY='devevelopmentsamplebullshitpassword',
+SECURITY_PASSWORD_SALT='ninjinshirishiri',
+# DATABASE=os.path.join(app.instance_path, 'stub.sqlite'),
+SQLALCHEMY_DATABASE_URI="postgresql+psycopg2://team5:1qazxsw2@localhost/team5db",
+JWT_TOKEN_LOCATION=["cookies", "headers"],
+)
+
+jwt = JWTManager(app)
+db = SQLAlchemy(app)
+
+##### Database
+
+class Product(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(), unique=True, nullable=False)
+  price = db.Column(db.Integer, nullable=False)
+  out_of_stock = db.Column(db.Boolean, nullable=False, default=False)
+  kind = db.Column(db.String(), nullable=False) # this is enum in fact
+  date = db.Column(db.Date) # null if kind == P
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  sid = db.Column(db.String(), nullable=False, unique=True)
+  password_hash = db.Column(db.String(), nullable=False)
+
+  def authenticate(self, pw):
+    return bcrypt.verify(pw, self.password_hash)
+
+# @app.before_first_request
+# def create_user():
+#   db.create_all()
+#   db.session.add(User(sid="e1704", password_hash=bcrypt.hash("password")))
+#   db.
+#   db.session.commit()
+
+##### Routes
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+  return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+  if request.method == "GET":
+    return render_template("login.html")
+  
+  sid = request.form['sid']
+  pw = request.form['password']
+  error = None
+  user = None
+
+  if not sid:
+    error = '学籍番号を入力してください'
+  elif not pw:
+    error = 'パスワードを入力してください'
+  else:
+    user = User.query.filter_by(sid=sid).first()
+    if not user or not user.authenticate(pw):
+      error = "パスワードか学籍番号が間違っています。"
+  
+  if error is None:
+    access_token = create_access_token(identity=sid)
+    return redirect(url_for('index'))
+
+  flash(error)
+  return render_template('login.html')
+
+@app.route('/logout', methods=["POST"])
+def logout():
+  pass
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+  if request.method == 'GET':
+    return render_template('register.html')
+  
+  sid = request.form['sid']
+  pw = request.form['password']
+  error = None
+
+  if not sid:
+    error = '学籍番号を入力してください'
+  elif not pw:
+    error = 'パスワードを入力してください'
+  elif User.query.filter_by(sid=sid).first() is not None:
+    error = '登録済みです。<a href="/login">ログインはこちら</a>'
+  
+  if error is None:
+    db.session.add(User(sid=sid, password_hash=bcrypt.hash(pw)))
+    db.session.commit()
+    return redirect(url_for('login'))
+
+  flash(error)
+  return render_template('register.html')
+
+# @app.route('')
+
+
+# メニュー一覧
+@app.route("/menu")
+def menu():
+  products = Product.query.all()
+  return render_template("menu.html", products=products)
+
+
+# メニューの個別ページ
+@app.route("/menu/<id>")
+def product(id):
+  product = Product.query.get(id)
+  return render_template("product.html", product=product)
+
+# マイページ
+@app.route("/profile")
+@jwt_required()
+def profile():
+  sid = get_jwt_identity()
+  user = User.query.filter_by(sid=sid).fetchone()
+  
+  if not User:
+    flash('ユーザーが見つかりません')
+    return redirect('index')
+
+  return render_template("profile.html", user=user)
+
+# 出費を見られるページ
+@app.route("/profile/expense")
+@jwt_required()
+def expense():
+  return render_template("expense.html")
+
+##### API
+
+# @app.route("/api/menu/update")
+
+# @app.route("/api/profile/edit", )
+
+@app.route("/api/dummy/get")
+def get():
+  return { data: 'hogehoge' }
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=8085)
